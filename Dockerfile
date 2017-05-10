4753b5c2f0158463c78e87234dc8723abe38e936
洪灵 <hongling@honglingdeMacBook-Pro.local>
2017-05-10 17:23:09 +0800

    the server must specify the server field as 0.0.0.0 inside docker container but the local must use the server field as it's www addr
    so
    the local config and the server config should be diffrent.
    split the ss.json to server.json and local.json

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM mritd/shadowsocks:3.0.6
 
 
 RUN apk upgrade --no-cache &&\
-    apk add --no-cache supervisor
+    apk add --no-cache supervisor &&\
+    apk add --no-cache curl
 
 COPY ./conf.d /conf.d
 COPY ./templates /templates
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-CONF_TEMPLATE="/templates/ss.json"
-CONF_DEFAULT="/conf.d/ss.json"
-
+CONF_TEMPLATE="/conf.d/template.json"
+CONF_SERVER="/conf.d/server.json"
+CONF_LOCAL="/conf.d/local.json"
 
 function genPwd()
 {
-  echo `tr -cd A-Za-z0-9 < /dev/urandom | head -c${1:-10}`
+  echo `tr -cd A-Za-z0-9 < /dev/urandom | head -c${1:-16}`
 }
 
-function makeDefaultConf() 
+function makeConf() 
 {
-  cp -f $CONF_TEMPLATE $CONF_DEFAULT 
-  pwd=`genPwd`;
-  sed -i "s/<%PASSWORD%>/${pwd}/" $CONF_DEFAULT;
+  cp -f $CONF_TEMPLATE $1
+  sed -i "s/<%SERVER%>/$2/" $1
+  sed -i "s/<%PASSWORD%>/$3/" $1
 }
 
-if [ ! -f $CONF_DEFAULT ]; then
-  makeDefaultConf;
+if [ ! -f $CONF_TEMPLATE ]; then
+  cp /templates/template.json $CONF_TEMPLATE
+fi
+
+
+if [ ! -f $CONF_SERVER ]; then
+  pwd=`genPwd`;
+  makeConf  $CONF_SERVER 0.0.0.0 $pwd;
+
+  server=`curl www.query-ip.com -s`;
+  makeConf $CONF_LOCAL $server $pwd;
 fi
 
 supervisord
