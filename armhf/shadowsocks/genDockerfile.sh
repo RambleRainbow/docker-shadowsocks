bfc48a8ba8968e59fc3b8b90d76475e04dac063a
洪灵 <hongling@honglingdeMacBook-Pro.local>
2017-05-17 11:23:47 +0800

    修改错误

--- armhf/shadowsocks/genDockerfile.sh
+++ armhf/shadowsocks/genDockerfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -f ../../Dockerfile ./Dockerfile
-sed -i "" "s/mritd\/shadowsocks:3\.0\.6/warmworm\/shadowsocks-armhf-base:3\.0\.6/" Dockerfile
+sed -i "" "s/mritd\/shadowsocks:3\.0\.6/warmworm\/shadowsocks_armhf_base:3\.0\.6/" Dockerfile
