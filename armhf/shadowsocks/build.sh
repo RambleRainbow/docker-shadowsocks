4b715fb87a22a1b8ea461e73961260e5e30f4c83
洪灵 <hongling@honglingdeMacBook-Pro.local>
2017-05-17 16:20:11 +0800

    manual images for armhf

--- armhf/shadowsocks/build.sh
+++ armhf/shadowsocks/build.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+echo "1.copy and rebase dockerfile"
 cp -f ../../Dockerfile ./Dockerfile
 sed -i "" "s/mritd\/shadowsocks:3\.0\.6/warmworm\/shadowsocks_armhf_base:3\.0\.6/" Dockerfile
+
+echo "2.copy docker images files"
+rm -rf files
+cp -r ../../files .
+
+echo "3.please input images tag:"
+read imgTag
+docker build -t warmworm/shadowsocks_armhf:${imgTag} .
+
+echo "4.push image"
+docker push warmworm/shadowsocks_armhf:${imgTag}
+
+echo "5. clean"
+rm -rf Dockerfile files
+
+echo "6.finshed"
+
