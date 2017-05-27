ddc1aee82bf68719abedd068aee83bfc02a3737a
洪灵 <hongling@honglingdeMacBook-Pro.local>
2017-05-27 13:46:55 +0800

    修复ARMHF的打包提交脚本的BUG

--- buildarm.sh
+++ buildarm.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 echo "1.copy and rebase dockerfile"
-cp -f ../../Dockerfile ./Dockerfile_arm
-sed -i "" "s/mritd\/shadowsocks:3\.0\.6/warmworm\/shadowsocks_armhf_base:3\.0\.6/" Dockerfile
+cp -f Dockerfile Dockerfile_arm
+sed -i "" "s/mritd\/shadowsocks:3\.0\.6/warmworm\/shadowsocks_armhf_base:3\.0\.6/" Dockerfile_arm
 
-echo "2.copy docker images files"
-rm -rf files
-cp -r ../../files .
+echo "2.input images tag:"
+read -p "please input tag(like 0.0.x):  " imgTag 
+docker build -t warmworm/shadowsocks_armhf:${imgTag} . -f Dockerfile_arm
 
-echo "3.please input images tag:"
-read imgTag
-docker build -t warmworm/shadowsocks_armhf:${imgTag} -f Dockerfile_arm
-
-echo "4.push image"
+echo "3.push image"
 docker push warmworm/shadowsocks_armhf:${imgTag}
 
-echo "5. clean"
-rm -rf Dockerfile_arm files
+echo "4. clean"
+rm -rf Dockerfile_arm
 
-echo "6.finshed"
+echo "5.finshed"
 
